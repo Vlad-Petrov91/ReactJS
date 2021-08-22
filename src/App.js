66772cc1a241978a908915e564b707402b33c1ac
Vlad-Petrov91 <vladison@list.ru>
2021-08-22 20:31:49 +0300

    lesson-2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,102 @@
-import { Message } from "./components/Message";
+
+import { useState, useEffect } from "react"
+// import { Button } from "./components/Button"
+// import { Input } from "./components/Input"
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <Message text="Hello React" />
-    </div>
-  );
-}
 
-export default App;
+
+
+  // const createMessage = (message, user) => ({
+  //   message,
+  //   user
+  // });
+
+  const madeMessage = (message, user = "Vlad") => ({
+    message,
+    user
+  });
+
+  const [value, setValue] = useState('');
+
+  const onChange = e => {
+    setValue(e.target.value)
+  }
+
+  const [message, messageList] = useState([]);
+
+
+  const addMessage = (...items1) => {
+    messageList((message) => {
+      return [
+        ...message,
+        ...items1,
+      ]
+    })
+
+  }
+
+
+
+  return (
+
+
+    <div className="App">
+
+
+      {/* <Input props={(event) => { console.dir(event.target.value) }} /> */}
+
+      <input type="text" value={value} onChange={onChange} />
+
+      {/* <Button text="SendMessage" func={() => {
+        addMessage(madeMessage('user', 'sdsd'));
+
+      }} /> */}
+
+
+      <button onClick={() => {
+        addMessage(madeMessage(value));
+
+      }} >SendMessage</button>
+
+
+
+      <ul>
+
+        {
+          message.map(({ message, user }) => <li>
+            <h3>
+              message: {message}
+            </h3>
+            <p>
+              user: {user}
+            </p>
+          </li>)
+        }
+      </ul>
+
+
+
+
+
+      {/* <button
+        onClick={() => {
+          addMessage(createMessage('user', Date.now()));
+        }}>
+        append1
+      </button> */}
+
+
+    </div>
+
+
+
+
+  )
+
+}
+export default App
---
+++ src/components/Input/index.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+// export const Input = ({ props }) => {
+
+
+
+//    return (
+//       <div>
+//          <input className="inputMessage" onChange={props} type="text"></input>
+//          {/* <input value={title} onChange={onChange} type="text" /> */}
+//       </div>
+//    );
+// }
